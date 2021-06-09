c96c452973c76aba841d01a0acea37f3d2ee1085
Gean Lucas <gean.lfpsa@gmail.com>
2021-06-08 23:04:58 -0300

    Added load more button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,7 +61,11 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
